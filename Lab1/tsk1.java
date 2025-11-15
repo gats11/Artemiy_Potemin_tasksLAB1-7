8689a2f32d4b991716c3015748e01a4140db9dae
gats11 <my2zapaska228@gmail.com>
2025-11-15 13:24:22 +0300

    Create tsk1.java

---
+++ Lab1/tsk1.java
@@ -0,0 +1,9 @@
+//Создание списка на языке Java
+string[] li = new string[] {"a","1","b","2","c","3"};
+
+//Создание stack на языке Java
+MyStack stack = new MyStack();
+stack.push("a");
+stack.push("b");
+
+stack.push("c");
